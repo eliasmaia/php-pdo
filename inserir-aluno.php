82621896c40f66004263d73c391fffb8f280fef8
Elias Moraes <eeliasmaia@gmail.com>
2021-05-25 16:00:17 -0300

    inserindo com exec

---
+++ inserir-aluno.php
@@ -0,0 +1,14 @@
+<?php
+
+use Alura\Pdo\Domain\Model\Student;
+
+require_once 'vendor/autoload.php';
+
+$caminhoBanco = __DIR__ . '/banco.sqlite';
+$pdo = new PDO('sqlite:' . $caminhoBanco);
+
+$student = new Student(null, 'Elias Neto', new DateTimeImmutable('1994-03-28'));
+
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES ('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}');";
+
+echo var_dump($pdo->exec($sqlInsert));
