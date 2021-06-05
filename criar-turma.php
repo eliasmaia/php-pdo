0dbb88bf9373a4add6a9261fc05bd74bde68b9ab
Elias Moraes <eeliasmaia@gmail.com>
2021-06-05 16:05:28 -0300

    iniciando com transacoes

---
+++ criar-turma.php
@@ -0,0 +1,28 @@
+<?php
+
+use Alura\Pdo\Domain\Model\Student;
+use Alura\Pdo\Infrastructure\Persistence\ConnectionCreator;
+use Alura\Pdo\Infrastructure\Repository\PdoStudentRepository;
+
+require_once 'vendor/autoload.php';
+
+$connection = ConnectionCreator::createConnection();
+$studentRepository = new PdoStudentRepository($connection);
+
+//realizar processo de definição de turma
+$connection->beginTransaction();
+$aStudent = new Student(
+    null,
+    'Nico Steppat',
+    new DateTimeImmutable('1985-05-01')
+);
+$studentRepository->save($aStudent);
+
+$anotherStudent = new Student(
+    null,
+    'Nico Rosberg',
+    new DateTimeImmutable('1985-05-01')
+);
+$studentRepository()->save($anotherStudent);
+
+$connection->commit();
