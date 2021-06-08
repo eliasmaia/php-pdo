7391dcbeda59aeca4606c60b089822209ed9b609
Elias Moraes <eeliasmaia@gmail.com>
2021-06-08 11:13:30 -0300

    problema n+1

--- src/Domain/Repository/StudentRepository.php
+++ src/Domain/Repository/StudentRepository.php
@@ -8,6 +8,7 @@ interface StudentRepository
 {
 
     public function allStudents(): array;
+    public function studentsWithPhones(): array;
     public function studentsBirthAt(\DateTimeInterface $birthdate): array;
     public function save(Student $student): bool;
     public function remove(Student $student): bool;
